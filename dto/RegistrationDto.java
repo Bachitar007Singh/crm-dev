ee8eb044ce55a09647ca58ebb30c98eda1bf5c6f
Bachitar007Singh <bachitar007singh@gmail.com>
2025-03-06 00:48:25 +0530

    this is initial files or basic controller,dto,model,service setup for crm

---
+++ dto/RegistrationDto.java
@@ -0,0 +1,68 @@
+package com.app.crm.dto;
+
+public class RegistrationDto {
+
+	private String name;
+	private String email;
+	private String contactno;
+	private String otp;
+	private String state;
+	private String city;
+	private String institution;
+	private String course;
+	private String remark;
+	public String getRemark() {
+		return remark;
+	}
+	public void setRemark(String remark) {
+		this.remark = remark;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getContactno() {
+		return contactno;
+	}
+	public void setContactno(String contactno) {
+		this.contactno = contactno;
+	}
+	public String getOtp() {
+		return otp;
+	}
+	public void setOtp(String otp) {
+		this.otp = otp;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getInstitution() {
+		return institution;
+	}
+	public void setInstitution(String institution) {
+		this.institution = institution;
+	}
+	public String getCourse() {
+		return course;
+	}
+	public void setCourse(String course) {
+		this.course = course;
+	}
+}
