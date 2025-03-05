a368f3a68ab81b1be7c42d1c333834485c10737f
Bachitar007Singh <bachitar007singh@gmail.com>
2025-03-06 01:02:26 +0530

    updated crm with more nav bar option and pages

--- src/main/java/com/app/crm/service/CounselorRepository.java
+++
@@ -1,7 +0,0 @@
-package com.app.crm.service;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.app.crm.model.Counselor;
-
-public interface CounselorRepository extends JpaRepository<Counselor, Integer> {
-}
